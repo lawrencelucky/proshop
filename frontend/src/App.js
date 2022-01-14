97c3cfa5d72cd49e97926ef4a8e0028f7ac2de4a
lawrencelucky <lawrencelucky@gmail.com>
2022-01-14 20:05:17 +0100

    React router dom setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,9 @@
+const ProductScreen = () => {
+  return (
+    <div>
+      <h3>Product</h3>
+    </div>
+  );
+};
+
+export default ProductScreen;
